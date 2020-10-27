b7b660f48d547c19e816b5987ebb303ed59934c8
sashman <sashman90@gmail.com>
2020-10-27 22:13:40 +0000

    CSV upload and decode

--- lib/my_attire_demo_api_web/controllers/attire_controller.ex
+++ lib/my_attire_demo_api_web/controllers/attire_controller.ex
@@ -3,7 +3,15 @@ defmodule MyAttireDemoApiWeb.AttireController do
 
   action_fallback MyAttireDemoApiWeb.FallbackController
 
-  def bulk_upload(conn, _params) do
+  def bulk_upload(conn, %{"attire" => file}) do
+
+    file.path
+      |> File.stream!()
+      |> CSV.decode(headers: true)
+      |> Stream.take(5)
+      |> Enum.to_list()
+      |> IO.inspect()
+
     conn
     |> json("ok")
   end
